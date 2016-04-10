b9dad400e9af8c153e66cbb3fdbe6f0b0963c9c8
ratajc <fr2355@student.uni-lj.si>
2016-04-10 08:18:05 +0000

    Zaključek dela na I. nalogi

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -96,9 +96,15 @@ $(document).ready(function() {
 
   socket.on('uporabniki', function(uporabniki) {
     $('#seznam-uporabnikov').empty();
+    
     for (var i=0; i < uporabniki.length; i++) {
       $('#seznam-uporabnikov').append(divElementEnostavniTekst(uporabniki[i]));
     }
+    
+    $('#seznam-uporabnikov div').click(function() {
+      $('#poslji-sporocilo').val('/zasebno "' + $(this).text() + '" ');
+      $('#poslji-sporocilo').focus();
+    });
   });
 
   setInterval(function() {
